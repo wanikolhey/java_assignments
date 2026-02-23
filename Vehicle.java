1da5032214b663665ef53abc7df80679b12ecf0f
wani <waniikolhey@gmail.com>
2026-02-23 20:14:33 +0530

    exp 2

---
+++ Vehicle.java
@@ -0,0 +1,81 @@
+import java.time.Year;
+public class Vehicle {
+    // Public data members
+    public String brandName;
+    public String modelName;
+    public double price;
+    public String color;
+    public Year mfgYear;
+    public char fuelType; // P-Petrol, D-Diesel, E-Electric, H-Hybrid, C-CNG
+    // Private data members
+    private String mfgCode;
+    private String regNo;
+    // Getters and setters
+    public void setMfgCode(String mCode) {
+        mfgCode = mCode;
+    }
+    public String getMfgCode() {
+        return mfgCode;
+    }
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+    public String getRegNo() {
+        return regNo;
+    }
+    // Default constructor
+    public Vehicle() {
+        brandName = "Hyundai";
+        modelName = "i10";
+        price = 500000;
+        color = "Silver";
+        mfgYear = Year.of(2020);
+        fuelType = 'P';
+        mfgCode = "HYN12345";
+        regNo = "MH12AA1111";
+    }
+    // Parameterized constructor
+    public Vehicle(String brandName, String modelName, double price, String mfgCode) {
+        this.brandName = brandName;
+        this.modelName = modelName;
+        this.price = price;
+        this.mfgCode = mfgCode;
+    }
+    // Copy constructor
+    public Vehicle(Vehicle v) {
+        brandName = v.brandName;
+        modelName = v.modelName;
+        price = v.price;
+        color = v.color;
+        mfgYear = v.mfgYear;
+        fuelType = v.fuelType;
+        mfgCode = v.mfgCode;
+        regNo = v.regNo;
+    }
+    // Required methods
+    public void start() {
+        System.out.println("Vehicle started");
+    }
+    public void stop() {
+        System.out.println("Vehicle stopped");
+    }
+    public void drive() {
+        System.out.println("Vehicle is driving");
+    }
+    public void changeSpeed(int newSpeed) {
+        System.out.println("Speed changed to " + newSpeed);
+    }
+    // Mileage depends on fuel type and distance
+    public double calcMileage(double distance) {
+        double efficiency;
+        switch (fuelType) {
+            case 'P': efficiency = 15; break;   // Petrol
+            case 'D': efficiency = 20; break;   // Diesel
+            case 'E': efficiency = 120; break;  // Electric
+            case 'H': efficiency = 25; break;   // Hybrid
+            case 'C': efficiency = 22; break;   // CNG
+            default: efficiency = 0;
+        }
+        return distance / efficiency;
+    }
+}
---
+++ VehicleMain.java
@@ -0,0 +1,65 @@
+import java.time.Year;
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle("Honda", "City", 1200000, "HON123");
+        v2.color = "White";
+        v2.fuelType = 'P';
+        v2.mfgYear = Year.of(2021);
+        v2.setRegNo("MH12AB2222");
+        Vehicle v3 = new Vehicle("Maruti", "Swift", 800000, "MAR456");
+        v3.color = "Red";
+        v3.fuelType = 'P';
+        v3.mfgYear = Year.of(2020);
+        v3.setRegNo("MH14CD3333");
+        Vehicle v4 = new Vehicle("Hyundai", "Creta", 1600000, "HYN789");
+        v4.color = "Grey";
+        v4.fuelType = 'D';
+        v4.mfgYear = Year.of(2022);
+        v4.setRegNo("KA01EF4444");
+        Vehicle v5 = new Vehicle("Tata", "Nexon", 1400000, "TAT321");
+        v5.color = "Blue";
+        v5.fuelType = 'D';
+        v5.mfgYear = Year.of(2021);
+        v5.setRegNo("DL05GH5555");
+        Vehicle v6 = new Vehicle("Mercedes", "S-Class", 4500000, "MER654");
+        v6.color = "Black";
+        v6.fuelType = 'P';
+        v6.mfgYear = Year.of(2022);
+        v6.setRegNo("DL01IJ6666");
+        Vehicle v7 = new Vehicle("Tesla", "Model 3", 3500000, "TES999");
+        v7.color = "White";
+        v7.fuelType = 'E';
+        v7.mfgYear = Year.of(2023);
+        v7.setRegNo("MH01KL7777");
+        Vehicle v8 = new Vehicle(v2);
+        v8.setMfgCode("HON888");
+        v8.setRegNo("MH12MN8888");
+        Vehicle v9 = new Vehicle(v4);
+        v9.setMfgCode("HYN999");
+        v9.setRegNo("KA01OP9999");
+        Vehicle v10 = new Vehicle(v7);
+        v10.setMfgCode("TES111");
+        v10.setRegNo("MH01QR1111");
+        Vehicle[] vehicles = {
+            v1, v2, v3, v4, v5,
+            v6, v7, v8, v9, v10
+        };
+        double distance = 300;
+        System.out.printf(
+            "%-10s %-12s %-6s %-12s %-10s%n",
+            "Brand", "Model", "Fuel", "Price", "Mileage"
+        );
+        System.out.println("--------------------------------------------------------------");
+        for (Vehicle v : vehicles) {
+            System.out.printf(
+                "%-10s %-12s %-6c %-12.2f %-10.2f%n",
+                v.brandName,
+                v.modelName,
+                v.fuelType,
+                v.price,
+                v.calcMileage(distance)
+            );
+        }
+    }
+}
